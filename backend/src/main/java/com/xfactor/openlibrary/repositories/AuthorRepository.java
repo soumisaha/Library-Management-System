3ea526d8b738646c2b9aa3803f22ec5dd3fd4690
Soumi Saha <sahasoumi1010@gmail.com>
2023-04-09 11:14:54 +0530

    librarymanagement

---
+++ backend/src/main/java/com/xfactor/openlibrary/repositories/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.xfactor.openlibrary.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xfactor.openlibrary.domain.Author;
+
+public interface AuthorRepository extends JpaRepository<Author, Long>{
+    List<Author> findByName(String name);
+
+    List<Author> findById(String id);
+}
---
+++ backend/src/main/java/com/xfactor/openlibrary/repositories/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.xfactor.openlibrary.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xfactor.openlibrary.domain.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+    List<Student> findByName(String name);
+
+    List<Student> findById(String id);
+}
